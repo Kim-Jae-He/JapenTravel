a9d7b94560a320168e08a0f786d638e26091bb6b
이은이 <dldmsdl99@gmail.com>
2023-10-01 21:04:05 +0900

    항공권페이지작업중

--- pages/js/flight.js
+++ pages/js/flight.js
@@ -1,3 +1,16 @@
+let swiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 20,
+    slidesPerGroup:2,
+
+    navigation: {
+        nextEl : '.slideNext',
+        prevEl : '.slidePrev',
+    }
+});
+
+
 let moreBtn = document.querySelector('.item-container1-container');
 let more = document.querySelector('.item-container1 ul');
 moreBtn.addEventListener('mouseover', function(){
